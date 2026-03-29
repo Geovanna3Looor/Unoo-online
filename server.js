25574a5db173153700e074199ad7ce4cce90e40b
Geovanna3Looor <geovannaloora@gmail.com>
2026-03-29 18:50:27 -0500

    Create server.js

---
+++ server.js
@@ -0,0 +1,18 @@
+{
+  "name": "uno-game-online",
+  "version": "1.0.0",
+  "description": "Juego UNO online para 12 jugadores con voz y stickers",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js",
+    "dev": "nodemon server.js"
+  },
+  "dependencies": {
+    "express": "^4.18.2",
+    "socket.io": "^4.7.2",
+    "uuid": "^9.0.1"
+  },
+  "devDependencies": {
+    "nodemon": "^3.0.1"
+  }
+}
